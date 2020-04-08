10ca00080ffe1ee6b509006a1f736add8670e20c
Sean Kramer <seandominickramer@gmail.com>
2020-04-08 15:55:08 -0400

    Add parent selector; add function with object argument

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,20 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+/*grab parent element */
+const articles = document.querySelector('.articles');
+
+/*create function*/
+function makeArticles = ({title, date, firstParagraph, secondParagraph, thirdParagraph}) {
+
+  /*instantiate elements*/
+
+
+
+
+
+
+
+}
+
